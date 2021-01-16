94f550184b6ac766293ad24adaa4318fdd3d8ba6
Lester <marclester.k.tang@smretail.com>
2021-01-16 15:45:29 +0800

    Add card layout to Details page

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,4 +1,6 @@
 import React, {FC} from "react";
+import {Card, Container} from "react-bootstrap";
+import styles from "../Home/index.module.css";
 
 interface Props {
 
@@ -6,9 +8,17 @@ interface Props {
 
 const Details : FC<Props> = ({}) => {
     return <>
-        <div>
-            Detail
-        </div>
+        <Container className={`mb-4 ${styles.center}`}>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk
+                        of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Container>
     </>
 }
 
